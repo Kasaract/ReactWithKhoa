83893b7a70a39ed1348ed75d02adb1f3f657514f
Khoa Trang <khoatrang25@gmail.com>
2019-09-01 11:35:14 -0400

    task 1 update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from '../components/Home';
+import About from '../components/About';
+import Contact from '../components/Contact';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+      <div className="App">
+        <Home />
+        <About />
+        <Contact />  
+      </div>
+    );
 }
 
 export default App;
